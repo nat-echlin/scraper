ce74be32db01fb7066ee2f8d9d77ad29377081e2
nat-echlin <34217077+nat-echlin@users.noreply.github.com>
2021-06-27 19:53:13 +0100

    Update bbc.py
    implemented use via the console & not just putting in print lines
    implemented ability to follow up on a story (be given a url to it)

--- bbc.py
+++ bbc.py
@@ -5,6 +5,7 @@ _source = requests.get("https://www.bbc.co.uk/news/technology").text
 soup = BeautifulSoup(_source, 'lxml')
 stories = []
 
+
 class Article:
     def __init__(self, title, link, isHeadline=False):
         self.title = title
@@ -26,22 +27,56 @@ def scrapeHeadline():
 
 def scrapeOthers():
     otherStoriesList = list(set(soup.find_all('a', attrs={"class":"gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-pica-bold nw-o-link-split__anchor"})))
-    for storySoup in otherStoriesList:
+    for index, storySoup in enumerate(otherStoriesList):
         regexp = "^Video [0-9]+"
-        if len(re.findall(regexp, storySoup.text)) == 0:
-            title = storySoup.text
-        else:
+        if re.search(regexp, storySoup.text):
+            # title = storySoup.text
             title = storySoup.text.split('seconds')[1]
+        else:
+            # title = storySoup.text.split('seconds')[1]
+            title = storySoup.text
+            
+
+
         _url = storySoup["href"]
-        link = f"https://www.bbc.co.uk{_url}"
+        if re.search('playlist', _url):
+            link = _url
+        else:        
+            link = f"https://www.bbc.co.uk{_url}"
         stories.append(
             Article(title, link)
         )
-    
+
+# Fn to print all titles, numbered 
+
 def printTitles():
-    for story in stories:
-        print(story.title)   
+    for number, story in enumerate(stories):
+        if story.isHeadline:
+            print(f"{number + 1}.   HEADLINE:   {story.title}\n")
+        else:
+            print(f"{number + 1}.    {story.title}\n")   
 
 
 # main story class:      gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-paragon-bold gs-u-mt+ nw-o-link-split__anchor
 # other storys class:    gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-pica-bold nw-o-link-split__anchor
+
+
+def init():
+    scrapeHeadline()
+    scrapeOthers()
+    printTitles()
+
+    def getFollowInput(maxStory):    
+        storyToFollowRaw = input("Please enter the number of the story you would like to follow up on!")
+        if re.search('[a-zA-Z]', storyToFollowRaw) or int(storyToFollowRaw) < 1 or int(storyToFollowRaw) > maxStory:
+            print(f"Please ONLY enter a number between 1 and {maxStory}")
+            getFollowInput(maxStory)
+        else:
+            return int(storyToFollowRaw)
+
+    toFollow = getFollowInput(len(stories))
+    print(stories[toFollow - 1].link)
+    
+init()
+    
+# address bug: link is wrong when story has a video
