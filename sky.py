4b3502bf169aabc466f06ce10239ebba764ffaef
nat <nat.echlin@gmail.com>
2021-06-30 13:11:09 +0100

    master

--- sky.py
+++ sky.py
@@ -9,29 +9,41 @@ soup = BeautifulSoup(_source, 'lxml')
 
 def getSkyHeadline():
     headlineSoup = soup.find("a", attrs={"class":"sdc-site-tile__headline-link"})
-    title, _url = headlineSoup.text, headlineSoup["href"]
+    title, _url = headlineSoup.span.text, headlineSoup["href"]
+    
+    
+    # print(headlineSoup.span.text)# REMOVE ME
+    # print("x")# REMOVE ME
+    # print(headlineSoup.text)# REMOVE ME
+
+    # title = title.strip("/n/t/r") # REMOVE ME
     link = f"https://news.sky.com{_url}"
     
     return Article(title, link, isHeadline=True)
     
-print(getSkyHeadline())
+getSkyHeadline() # remove me
+
+
+
+
+# print(getSkyHeadline())
 
 def getSkyOthers():
     othersSoup = soup.find_all("a", attrs={"class":"sdc-site-tile__headline-link"})
 
     # urls = [story["href"] for story in othersSoup]
 
-    noVideosList = []
+    listOfStoriesWithNoVideos = []
     for index, storySoup in enumerate(othersSoup):
         if not re.search("video", storySoup["href"]):
-            noVideosList.append(othersSoup[index])
+            listOfStoriesWithNoVideos.append(othersSoup[index])
         else:
             # if video, do nothing (ie dont add to new list)
             pass
 
     otherStories = []   # list of stories to return
-    for story in noVideosList:
-        title, _url = story.text, story["href"]
+    for story in listOfStoriesWithNoVideos:
+        title, _url = story.span.text, story["href"]
         link = f"https://news.sky.com{_url}"
         otherStories.append(
             Article(title, link)
