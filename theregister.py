fb0d16beee19699cc126f61aef99b951ffe220fe
nat <nat.echlin@gmail.com>
2021-07-01 12:30:17 +0100

    cont: need to work on register

--- theregister.py
+++ theregister.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests, re
 
+# s = requests.Session(   )
+
 _source = requests.get("").content
 soup = BeautifulSoup(_source, features="xml")
 
@@ -8,3 +10,5 @@ print(_source)
 
 
 # needs serious work here. xml feed seems to not work, error 403 : robot detected.
+# https://stackoverflow.com/questions/13030095/how-to-save-requests-python-cookies-to-a-file
+# https://docs.python-requests.org/en/latest/api/#sessionapi
