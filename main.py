c04de87fcbd436d7bab8734919d572cb84c990f1
nat <nat.echlin@gmail.com>
2021-07-06 23:58:29 +0100

    think iv got prev stories working! fix bbc bug

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ from register import getRegister
 
 stories = []
 
-def getStories(onlyNew=False):
-    def getAll(arr):
+def getAll(arr):
         arr.append(getSkyHeadline())
         for story in getSkyOthers():
             arr.append(story)
@@ -20,12 +19,12 @@ def getStories(onlyNew=False):
             arr.append(story)
 
         for story in getRegister():
-            stories.append(story)
+            arr.append(story)
 
+def getStories(onlyNew=False):
     if onlyNew == False:    # ie: get all stories
         getAll(stories)
         
-
     else:   # ie : get only the stories that have come up since last time it was ran
         with open('previousStories.txt', 'r') as prevStoriestxt:
             prevStoriesRaw = prevStoriestxt.read()
@@ -41,13 +40,6 @@ def getStories(onlyNew=False):
                 if isValid:
                     stories.append(story)
     
-    with open('previousStories.txt', 'a') as prevStoriestxt:
-            for story in stories:
-                prevStoriestxt.write(f"{story.title}\n")
-            
-            
-
-
 def printTitles():
     for number, story in enumerate(stories):
         if story.isHeadline:
@@ -55,31 +47,46 @@ def printTitles():
         elif not story.isHeadline:
             print(f"{number + 1}.    {story.title}\n")
 
+
+
 if __name__ == "__main__":
     def showStories(onlyNew=False):
         getStories(onlyNew)
-        printTitles()
-        def getFollowInput(maxStory):    
-            storyToFollowRaw = input("Please enter the number of the story you would like to follow up on!")
-            if re.search('[a-zA-Z]', storyToFollowRaw) or int(storyToFollowRaw) < 1 or int(storyToFollowRaw) > maxStory:
-                print(f"Please ONLY enter a number between 1 and {maxStory}")
-                return int(getFollowInput(maxStory))
-            else:
-                return int(storyToFollowRaw)
+        if len(stories) == 0 and onlyNew == True:
+            print("\nThere are no new stories! Come back soon! :)         ( or im broken )")
+        
+        else:
+            printTitles()
+            def getFollowInput(maxStory):    
+                storyToFollowRaw = input("Please enter the number of the story you would like to follow up on!")
+                if re.search('[a-zA-Z]', storyToFollowRaw) or int(storyToFollowRaw) < 1 or int(storyToFollowRaw) > maxStory:
+                    print(f"Please ONLY enter a number between 1 and {maxStory}")
+                    return int(getFollowInput(maxStory))
+                else:
+                    return int(storyToFollowRaw)
 
-        toFollow = getFollowInput(len(stories))
-        print(stories[toFollow - 1].link)
+            toFollow = getFollowInput(len(stories))
+            print(stories[toFollow - 1].link)
+
+
+        # this writes all stories currently on the sites to the previosstories txt
+
+        with open('previousStories.txt', 'w') as prevStoriestxt:
+            currentStories = []
+            getAll(currentStories)
+            for story in currentStories:
+                prevStoriestxt.write(f"{story.title}\n")
     
     def getMode():
-        mode = input("Select mode: \n1. Only show stories that have been published since last run (and are on the front page currently)\n2. Show all stories currently on the front page")
+        mode = input("Select mode: \n1. Only show stories that have been published since last run (and are on the front page currently)\n2. Show all stories currently on the front page\n")
         if re.search('[a-zA-Z]', mode):
             print(f"Please ONLY enter '1' or '2'")
             return int(getMode())
         else:
             return int(mode) 
 
-    # mode = getMode()   
-    mode = 1        # REMOVE ME     
+    mode = getMode()   
+    # mode = 1        # REMOVE ME     
     if mode == 1:        # new stories
         showStories(onlyNew=True)
 
